a591cdded7837062344ad12c16f6d782ecfab8d6
Matthew Turner <maturner01@gmail.com>
2018-06-21 23:38:26 -0700

    second run of funding experiment after pos rate fix

--- funding_trials_qsub.sh
+++ funding_trials_qsub.sh
@@ -3,7 +3,7 @@
 policy=$1
 awardAmount=$2
 
-./scimod-agency /scratch/mturner8/scimod/fundingExperiment \
+./scimod-agency /scratch/mturner8/scimod/fundingExperiment2 \
     --fprMutationRate=0.1 \
     --fprMutationMagnitude=0.01 \
     --policy=$policy \
