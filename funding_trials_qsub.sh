59045a3ac1c2f017c9c90cb067d96b62261e525d
Matthew Turner <maturner01@gmail.com>
2018-06-22 17:40:51 -0700

    modified funds experiment runner for new params

--- funding_trials_qsub.sh
+++ funding_trials_qsub.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+#
+# Wrapper around scimod-agency binary that Sun Grid Engine will not run bare.
+# 
+# Date: 19 Jun 2018
+# Author: Matthew A. Turner <maturner01@gmail.com>
+#
 
 policy=$1
 awardAmount=$2
 
-./scimod-agency /scratch/mturner8/scimod/fundingExperiment2 \
-    --fprMutationRate=0.1 \
+./scimod-agency /scratch/mturner8/scimod/fundingExperiment \
+    --fprMutationRate=0.025 \
     --fprMutationMagnitude=0.01 \
     --policy=$policy \
     --awardAmount=$awardAmount \
-    --initialFalsePositiveRate=0.5 \
+    --initialFalsePositiveRate=0.05 \
     --nTrials=100
--- run_funding_experiment.sh
+++ run_funding_experiment.sh
@@ -1,20 +1,16 @@
 ##
 # run_funding_experiment.sh
 #
-# 
+# Using this as a runner. Just modify the values in for loops to change
+# parameterizations.
 #
 # Date: 19 Jun 2018
 # Author: Matthew A. Turner <maturner01@gmail.com>
 #
-# Example:
-#
-# qsub -S /bin/bash -q fast.q -cwd -j y -V -l mem_free=96G \
-#     -pe smp 20 -N mutation_funding -o funding.log -e funding.err \
-#     ./run_funding_experiment.sh testData
 
 
 for policy in RANDOM PUBLICATIONS FPR; do
-    for awardAmount in  `seq 10 5 205`; do # 40 x 3 = 120 total
+    for awardAmount in `seq 1 20` 15 `seq 25 5 150`; do # 40 x 3 = 120 total
 
         echo "Running policy=$policy and awardAmount=$awardAmount"
 
