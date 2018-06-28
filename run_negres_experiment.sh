0eedbdbc017e176f966b590a1778eeda84cf59eb
Matthew Turner <maturner01@gmail.com>
2018-06-28 06:35:12 -0700

    changed negres for negativeResults-rerun

--- run_negres_experiment.sh
+++ run_negres_experiment.sh
@@ -10,8 +10,8 @@
 
 
     # for awardAmount in `seq 1 20` 15 `seq 25 5 150`; do # 40 x 3 = 120 total
-# for policy in RANDOM PUBLICATIONS; do
-for policy in FPR; do
+# for policy in FPR; do
+for policy in RANDOM PUBLICATIONS; do
     for awardAmount in 1 `seq 5 5 115` ; do # 40 x 3 = 120 total
 
         for publishNegativeResultRate in 0.7 0.8 0.9 1.0; do
