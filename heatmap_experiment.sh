0abe04649de5998f275ad8959f80ff3cf0946f23
Matthew Turner <maturner01@gmail.com>
2018-09-04 09:25:20 -0700

    slurm batch script json2hdf

--- heatmap_experiment.sh
+++ heatmap_experiment.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #SBATCH --job-name=scimod-agency
-#SBATCH -t 0-0:05
+#SBATCH -t 0-0:25
 #SBATCH -n 20
 #SBATCH -N 1
 #SBATCH --output=logs/scimod-agency_%A_%a.out
 #SBATCH --error=logs/scimod-agency_%A_%a.err
 #SBATCH -p fast.q
 
-PARAMS=`awk NR==$SLURM_ARRAY_TASK_ID heatmap_params_2.txt`
+PARAMS=`awk NR==$SLURM_ARRAY_TASK_ID heatmap_params.txt`
 
-./scimod-agency "/home/scratch/mturner8/scimod/heatmap-highres" \
+./scimod-agency "/home/scratch/mturner8/scimod/heatmap-highres-5M_its" \
     --fprMutationRate=0.025 \
     --fprMutationMagnitude=0.01 \
     --initialFalsePositiveRate=0.05 \
+    --nIter=5000000 \
     --nTrials=100 \
     --paramsList=$PARAMS
 
